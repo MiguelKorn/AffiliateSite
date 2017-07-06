008f686d783ce7d9a073dc2f8a0160ac34393f8f
Miguel Korn <20448@ma-web.nl>
2017-07-06 09:40:25 +0200

    [CHANGE] nav remove search bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,23 +7,9 @@ export default class Nav extends React.Component {
                 <div className="container-fluid">
                     {/*<!-- Brand and toggle get grouped for better mobile display -->*/}
                     <div className="navbar-header">
-                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                            <span className="sr-only">Toggle navigation</span>
-                            <span className="icon-bar"> </span>
-                            <span className="icon-bar"> </span>
-                            <span className="icon-bar"> </span>
-                        </button>
                         <a className="navbar-brand">Affiliate Site</a>
                     </div>
                     {/*<!-- Collect the nav links, forms, and other content for toggling -->*/}
-                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <form className="navbar-form navbar-left">
-                            <div className="form-group">
-                                <input type="text" className="form-control" placeholder="Search" />
-                            </div>
-                            <button type="submit" className="btn btn-default">Submit</button>
-                        </form>
-                    </div>
                 </div>
             </nav>
         )
